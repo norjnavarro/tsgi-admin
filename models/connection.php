fe9f9f4c6edfb4fec4f6290015929ef9f6ac1694
norjnavarro <jron.martin.navarro@gmail.com>
2020-10-16 15:28:45 +0800

    configured connection.php

--- models/connection.php
+++ models/connection.php
@@ -2,7 +2,7 @@
 	/*ini_set("display_errors", 0);
 	error_reporting(0);*/
 	
-	$db		= "heroku_30745d0bcc47450";
+	$db		= "heroku_30745d0bcc4745";
 	$user		= "b5d2c72a35cea7";
 	$pw		= "f03b33d2";
     	$server		= "us-cdbr-east-02.cleardb.com";
