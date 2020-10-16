fdab554f9e43d9615981d9017df5e9c85a607355
norjnavarro <jron.martin.navarro@gmail.com>
2020-10-16 15:00:17 +0800

    To be deployed to Heroku

---
+++ models/connection.php
@@ -0,0 +1,17 @@
+<?php
+	/*ini_set("display_errors", 0);
+	error_reporting(0);*/
+	
+	$db		= "heroku_30745d0bcc47450";
+	$user		= "b5d2c72a35cea7";
+	$pw		= "f03b33d2";
+    	$server		= "us-cdbr-east-02.cleardb.com";
+    
+	// Create connection
+	$con = new mysqli($server, $user, $pw, $db);
+
+	// Check connection
+	if ($con->connect_error) {
+		die('Connection failed: ' . $con->connect_error);
+	}
+?>
